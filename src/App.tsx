d232ac23ec5703ce81d57bd69b76add55fd39d60
Sargam <sargampoudel100@gmail.com>
2023-07-19 16:52:33 +0530

    :pencil2: fix(style): fix image and basic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { TypeForm } from './Components/TypeForm';
+import gdscLogo from './assets/gdscfetlogo.png';
+
 export default function App() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className='py-10 flex flex-col items-center'>
+      <h1 className='text-center text-3xl md:text-5xl font-bold text-[#de5246]'>
+        Core Team Application Form
+      </h1>
+      <img src={gdscLogo} draggable='false' className='w-96 py-20' />
+      <TypeForm />
     </main>
   );
 }
---
+++ src/Components/TypeForm.tsx
@@ -0,0 +1,17 @@
+import { PopupButton } from '@typeform/embed-react';
+
+export function TypeForm() {
+  return (
+    <PopupButton
+      onSubmit={(e) => {
+        console.log(e.responseId);
+      }}
+      id='jovDAo3R'
+      style={{ fontSize: 20 }}
+      className='middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+      data-ripple-light='true'
+    >
+      Apply Here
+    </PopupButton>
+  );
+}
