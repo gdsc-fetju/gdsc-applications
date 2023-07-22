fbd1b1d13db2f1c6bfb9bcc7261152d1e7b72348
Sargam <sargampoudel100@gmail.com>
2023-07-23 02:06:05 +0530

    :pencil2: fix(style): better styles to webpage

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ export default function App() {
       <h1 className='text-center text-3xl md:text-5xl font-bold text-[#de5246]'>
         Core Team Application Form
       </h1>
-      <img src={gdscLogo} draggable='false' className='py-10 w-96' />
-      <TypeForm />
+      {/* Card Section */}
+      <div className='flex flex-col items-center p-5 px-10 pb-10 m-5 mt-10 border-2 border-blue-300 rounded-xl'>
+        <img src={gdscLogo} draggable='false' className='py-10 w-96' />
+        <TypeForm />
+      </div>
       <Footer />
     </main>
   );
