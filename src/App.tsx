dfd1332bdc1c6873930c3f6b9ebc10c5806de461
Sargam <sargampoudel100@gmail.com>
2023-08-07 00:35:41 +0530

    :sparkles: feat: add google form redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import Footer from './Components/Footer';
-import { TypeForm } from './Components/TypeForm';
-import gdscLogo from './assets/gdscfetlogo.png';
+import { useEffect } from 'react';
 
 export default function App() {
-  return (
-    <main className='flex flex-col items-center justify-between h-screen py-10'>
-      <h1 className='text-center text-3xl md:text-5xl font-bold text-[#de5246]'>
-        Core Team Application Form
-      </h1>
-      {/* Card Section */}
-      <div className='flex flex-col items-center p-5 px-10 pb-10 m-5 mt-10 border-2 border-blue-300 rounded-xl'>
-        <img src={gdscLogo} draggable='false' className='py-10 w-96' />
-        <TypeForm />
-      </div>
-      <Footer />
-    </main>
-  );
+  useEffect(() => {
+    window.location.href = 'https://forms.gle/1k8Sfb4oULmvYHtL9';
+  }, []);
+
+  return null;
 }
