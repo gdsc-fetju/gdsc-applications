0f1cf819bdb65cbfc6caef09a4c3b01e2ac9d249
Sargam <sargampoudel100@gmail.com>
2023-08-23 21:03:24 +0530

    :speech_balloon: fix(link): Update the redirect link

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function App() {
   useEffect(() => {
-    window.location.href = 'https://forms.gle/1k8Sfb4oULmvYHtL9';
+    window.location.href = 'https://forms.gle/N9S6AjrVgS3susUb6';
   }, []);
 
   return null;
